ddbfc0132f1a112d0bf79cf67320b085b2013776
dschwarz <danielschwarz26@gmail.com>
2014-04-20 17:38:33 -0700

    Add templating, upload to appengine.

--- game/main.py
+++ game/main.py
@@ -5,51 +5,52 @@ from game import Game
 from players import *
 
 player_types = [RandomPlayer, RandomPlayer, RandomPlayer, RandomPlayer, RandomPlayer, RandomPlayer,
-	SmallSimulationPlayer, SmallTAGSimulationPlayer, SmallLAGSimulationPlayer,
-	LargeSimulationPlayer, LargeTAGSimulationPlayer, LargeLAGSimulationPlayer,
-	TAGPlayer, TightTAGPlayer, LooseTAGPlayer] 
+  SmallSimulationPlayer, SmallTAGSimulationPlayer, SmallLAGSimulationPlayer,
+  LargeSimulationPlayer, LargeTAGSimulationPlayer, LargeLAGSimulationPlayer,
+  TAGPlayer, TightTAGPlayer, LooseTAGPlayer] 
 
 #Returns a random set of players of size table_size
 def get_players(table_size, starting_chips, results):
-	players = []
-	for i in range(table_size):
-		player_type = random.choice(player_types)
-		#Give each player a unique name.
-		name = player_type.__name__ + str(i)
-		player = player_type(starting_chips, name)
-		players.append(player)
-		results[player_type.__name__]['num_instances'] += 1
-	return players, results
+  players = []
+  for i in range(table_size):
+    player_type = random.choice(player_types)
+    #Give each player a unique name.
+    name = player_type.__name__ + str(i)
+    player = player_type(starting_chips, name)
+    players.append(player)
+    results[player_type.__name__]['num_instances'] += 1
+  return players, results
 
 def tally_results(results_for_player, starting_chips):
-	return results_for_player['final_chips'] - starting_chips * (
-		results_for_player['num_instances'] + results_for_player['num_rebuys'])
+  return results_for_player['final_chips'] - starting_chips * (
+    results_for_player['num_instances'] + results_for_player['num_rebuys'])
 
 def run(num_iterations=1, num_orbits=1):
-	starting_chips = 200
-	table_sizes = range(3, 10)
-	results = {}
-	for name in [player.__name__ for player in player_types]:
-		results[name] = {'final_chips': 0, 'num_instances': 0, 'num_rebuys': 0, 'result': 0}
-	for _ in range(num_iterations):
-		for table_size in table_sizes:
-			players, results = get_players(table_size, starting_chips, results)
-			game = Game(players, stack_size = starting_chips, num_orbits = num_orbits, debug_level = 0)
-			game.play_game()
-			for i, player in enumerate(game.players):
-				#Discard identifying string appended to name to track each player type
-				name = player.__name__.rstrip('1234567890')
-				results[name]['final_chips'] += player.chips
-				results[name]['num_rebuys'] += game.rebuys[i]
-	print results
-	total_chips = 0
-	for name in results.keys():
-		results[name]['result'] = tally_results(results[name], starting_chips)
-	for name in sorted(results.keys(), key = lambda x: results[x]['result']):
-		print '%s ends with %d. Instances: %d, rebuys: %d' % (name,
-			results[name]['result'], results[name]['num_instances'], results[name]['num_rebuys'])
-		total_chips += results[name]['result']
-	print 'Total chips: %d' % total_chips
+  starting_chips = 200
+  table_sizes = range(3, 10)
+  results = {}
+  for name in [player.__name__ for player in player_types]:
+    results[name] = {'final_chips': 0, 'num_instances': 0, 'num_rebuys': 0, 'result': 0}
+  for _ in range(num_iterations):
+    for table_size in table_sizes:
+      players, results = get_players(table_size, starting_chips, results)
+      game = Game(players, stack_size = starting_chips, num_orbits = num_orbits, debug_level = 0)
+      game.play_game()
+      for i, player in enumerate(game.players):
+        #Discard identifying string appended to name to track each player type
+        name = player.__name__.rstrip('1234567890')
+        results[name]['final_chips'] += player.chips
+        results[name]['num_rebuys'] += game.rebuys[i]
+  total_chips = 0
+  for name in results.keys():
+    results[name]['result'] = tally_results(results[name], starting_chips)
+  for name in sorted(results.keys(), key = lambda x: results[x]['result']):
+    output = '%s ends with %d. Instances: %d, rebuys: %d' % (name,
+      results[name]['result'], results[name]['num_instances'], results[name]['num_rebuys']) 
+    print output
+    total_chips += results[name]['result']
+    return results
+  print 'Total chips: %d' % total_chips
 
 if __name__ == '__main__':
-	run()
+  run()
--- pokersimulator.py
+++ pokersimulator.py
@@ -3,26 +3,32 @@ import webapp2
 import jinja2
 import cgi
 
+from game import main
+
 JINJA_ENVIRONMENT = jinja2.Environment(
-	loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    	extensions=['jinja2.ext.autoescape'],
-    	autoescape=True)
+  loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 class MainPage(webapp2.RequestHandler):
-	
-	def get(self):
-		template = JINJA_ENVIRONMENT.get_template('home.html')
-        	template_values = {'hi': 'sup'}
-		self.response.write(template.render(template_values))
+
+  def get(self):
+    template = JINJA_ENVIRONMENT.get_template('home.html')
+    template_values = {} 
+    self.response.write(template.render(template_values))
 
 class SecondPage(webapp2.RequestHandler):
 
-	def post(self):
-		self.response.write('<!doctype html><html><body>You wrote:<pre>')
-       		self.response.write(cgi.escape(self.request.get('content')))
-        	self.response.write('</pre></body></html>')
+  def post(self):
+    self.response.write('<!doctype html><html><body>Running simulation.<pre>')
+    numTables = int(cgi.escape(self.request.get('numTables')))
+    numOrbits = int(cgi.escape(self.request.get('numOrbits')))
+    results = main.run(numTables, numOrbits)
+    template = JINJA_ENVIRONMENT.get_template('results.html')
+    template_values = {'results': results}
+    self.response.write(template.render(template_values))
 
 application = webapp2.WSGIApplication([
 	('/', MainPage),
-	('/sign', SecondPage),
+	('/results', SecondPage),
 ], debug = True)
